3be3b754745ec9a4c514824240bcdd4c431ed09b
anastasiiaboiko <anastacia.boiko@gmail.com>
2014-11-20 15:47:31 +0200

    Initial Commit

---
+++ Hypnosista/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosista
+//
+//  Created by Anastasia Boiko on 11/20/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
